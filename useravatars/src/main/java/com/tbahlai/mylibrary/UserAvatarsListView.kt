103feb21a803a9132d4184fb017129eb2c26b303
Tetiana Bahlai <tania.baglay@gmail.com>
2022-03-16 12:21:06 +0200

    initial commit

---
+++ useravatars/src/main/java/com/tbahlai/mylibrary/UserAvatarsListView.kt
@@ -0,0 +1,26 @@
+package com.tbahlai.mylibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.tbahlai.mylibrary.databinding.ViewUserAvatarsListBinding
+
+class UserAvatarsListView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val binding by viewBinding(ViewUserAvatarsListBinding::bind)
+
+    init {
+        View.inflate(context, R.layout.view_user_avatars_list, this)
+        setupViews()
+    }
+
+    private fun setupViews() {
+
+    }
+}
